64b99d2edfc1e8a3d834b7af45d30b8144bfb745
KrishivS <krishivs789@gmail.com>
2021-08-24 21:41:35 +0530

    first commit

--- todolist/settings.py
+++ todolist/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5whdhg-(ae$q%o7fl$sogjt9ie%95*k6s76!2tph%6orq8*u%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://todo-crm1.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
